c4d46b6e2a47582869ccc1c223a1cfad85c61376
KRAILERKM <krailerk_man@hotmail.com>
2020-10-04 22:32:01 +0700

    10-4-2020 Update some security

--- rpas.bash
+++ rpas.bash
@@ -3,16 +3,16 @@
 ##############################Generator Password####################################
 ftppass='date | md5sum | cut -c 1-8'
 today='date +"%D"'
-echo -e "Dear All,\n\n\tNew password FTP KSC @USA to day $today information below.\n\t**IP : 113.21.240.138\n\t**Account : ksc-customer\n\t**Password : $ftppass\n\nThank You." > /tmp/change_password_ftp.txt
-echo $ftppass | passwd ksc-customer --stdin
-echo "Change Password: ksc-customer [Done.]"
+echo -e "Dear All,\n\n\tNew password FTP @USA to day $today information below.\n\t**IP : 192.168.0.1\n\t**Account :my-customer\n\t**Password : $ftppass\n\nThank You." > /tmp/change_password_ftp.txt
+echo $ftppass | passwd my-customer --stdin
+echo "Change Password: my-customer [Done.]"
 
 #################################Clear Content######################################
 rm -rf /home/ksc-customer/*
 echo "Clear Content: /home/ksc-customer [Done.]"
 
 #################################Send Password######################################
-mailx -s "Reset Password Account KSC FTP @USA" -r sysadmin@ksc.net -c sysadmin@ksc.net network@ksc.net < /tmp/change_password_ftp.txt
-echo "Send Mail: sysadmin@ksc.net [Done.]"
+mailx -s "Reset Password Account FTP @USA" -r sysadmin@my.net -c sysadmin@my.net network@my.net < /tmp/change_password_ftp.txt
+echo "Send Mail: sysadmin@my.net [Done.]"
 
 exit
